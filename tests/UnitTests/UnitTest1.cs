475acc0fa1e9569b14839d3788980f47b88fd906
Srujana Gattu <S533622@NWMISSOURI.EDU>
2019-02-07 12:52:56 -0600

    Added test functions and helper

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -5,10 +5,36 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        
+        [Theory]
+        [InlineData("erujana")]
+        [InlineData("anny")]
+        [InlineData("emme")]
+        public void StartingWithVowel(String n)
         {
+            Assert.True(IsVowel(n));
+        }
 
+        bool IsVowel(String  n)
+        {
+            if (n[0] == 'a' || n[0] == 'e' || n[0] == 'i' || n[0] == 'o' || n[0] == 'u') {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        [Fact]
+        public void checkingVowelwithWords()
+        {
+            bool expected = true;
+            var str = "and";
+   
+            var actual = IsVowel(str);
+            Assert.Equal(expected, actual);
         }
     }
 }
